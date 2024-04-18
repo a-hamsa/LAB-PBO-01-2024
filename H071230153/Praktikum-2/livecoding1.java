b7baba037eda98f91186c4b2ac2b2a218955440a
Dworzt <ziworth05@gmail.com>
2024-04-18 20:50:52 +0800

    H071231053 | Menambahkan Tugas praktikum 3

---
+++ H071230153/Praktikum-2/livecoding1.java
@@ -0,0 +1,28 @@
+public class livecoding1 {
+    String name;
+    int age;
+    Boolean isMale;
+
+    public String getName() {
+        return name;
+    }
+    public int getAge() {
+        return age;
+    }
+    public Boolean getIsMale() {
+        return isMale;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+    public void setIsMale(Boolean isMale) {
+        this.isMale = isMale;
+    }
+    
+
+}
+    
