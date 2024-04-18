705df42f7c36646e3c67ddb2ca761458a2029095
AnNauraErwana <putrianed23h@student.unhas.ac.id>
2024-04-18 13:15:55 +0800

    Tuprak 3 & 4

---
+++ H071231051/Praktikum 4/SelfUtils.java
@@ -0,0 +1,6 @@
+public class SelfUtils {
+    public static void displaySelfData() {
+        System.out.println("Nama : An Naura Erwana Dwi Putri");
+        System.out.println("NIM  : H071231051");
+    }
+}
